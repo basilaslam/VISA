322a8e49c813870962ced764547418abb5b3b326
basilaslam <basilaslamnp@gmail.com>
2023-10-29 08:56:45 +0530

    ready to deploy

--- app/admin/(dashboard)/dashboard/application/[id]/page.tsx
+++ app/admin/(dashboard)/dashboard/application/[id]/page.tsx
@@ -202,7 +202,7 @@ export const Application:FC<pageProps> = ({params}) =>{
 
 
 
-          {!application?.admin_pdf&&<div className="col-span-6 sm:col-span-3">
+          {<div className="col-span-6 sm:col-span-3">
             <label
               htmlFor="document"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -225,7 +225,7 @@ export const Application:FC<pageProps> = ({params}) =>{
 
 
           <div className="w-full flex justify-center items-center mt-14">
-            <Button disabled={application?.admin_pdf? true: false} variant={"primary"} className="mx-auto font-bold"><span>Update</span></Button>
+            <Button variant={"primary"} className="mx-auto font-bold"><span>Update</span></Button>
           </div>
 
         </form>
